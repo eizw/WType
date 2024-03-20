9a2bf9b840611bd186ead39cd22fae7d1a68dcc5
eizw <hamzahamri2005@gmail.com>
2024-03-20 18:10:01 +0800

    Rearranged components - Board now resides in a central game container

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WordComponent } from './word/word.component';
 import { BoardComponent } from './board/board.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +16,23 @@ import { FooterComponent } from './footer/footer.component';
     BoardComponent,
     HeaderComponent,
     FooterComponent,
+    HttpClientModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  private word_url: string = 'http://localhost:8000/words'
+
   title = 'frontend';
   gameTime: number = 15;
   public words!: string[];
+
+  word_queue!: string[]
+
+  @ViewChild('gameBoard') gameBoard: any;
+
+  constructor (private http: HttpClient) {}
 
   ngOnInit(): void {
   }
